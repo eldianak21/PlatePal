0ee7c1ca6e3cbf07100f2bd50cc0d9ccd85f095e
HELU23ALEX <HELINAAALEMU@gmail.com>
2025-02-19 11:20:59 +0300

    firebase setup

---
+++ nom-nom/src/firebase.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCSgd9e2CDV40pciOrJZPCwAywGynNKQc",
+  authDomain: "platepal-3cdef.firebaseapp.com",
+  projectId: "platepal-3cdef",
+  storageBucket: "platepal-3cdef.firebasestorage.app",
+  messagingSenderId: "568482755133",
+  appId: "1:568482755133:web:60bc7e607133322fd33054",
+  measurementId: "G-8KGZRSFLE3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
